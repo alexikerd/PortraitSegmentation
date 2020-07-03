545dbb186a8e7dc3a996c47a912c995bbbc0e412
alexikerd <41974772+alexikerd@users.noreply.github.com>
2020-07-03 14:50:10 -0500

    Add files via upload

--- Backend.py
+++ Backend.py
@@ -16,14 +16,13 @@ from torch.utils.data import Dataset, DataLoader
 
 
 
-class ImageDataset(Dataset):
+class T_ImageDataset(Dataset):
 
-    def __init__(self,target,test=False,transform=None):
+    def __init__(self,test=False,transform=None):
         self.root = path.abspath(path.curdir)
         self.transform = transform
         self.traintest = 'test' if test else 'train'
         self.data = os.listdir(self.root + f'/data/{self.traintest}/')
-        self.target = target
         
     def __len__(self):
         return len(self.data)
@@ -34,11 +33,10 @@ class ImageDataset(Dataset):
             index = index.tolist()
 
         item = Image.open(self.root + f'/data/{self.traintest}/' + self.data[index])
-        target = Image.open(self.root + f'/{self.target}/{self.traintest}/' + self.data[index].replace('data',self.target))
+        target = Image.open(self.root + f'/trimap/{self.traintest}/' + self.data[index].replace("data","trimap"))
         
         if self.transform:
             item = self.transform(item)
-            # target = self.transform(target)
             target = transforms.ToTensor()(target)*255
 
         
@@ -47,3 +45,124 @@ class ImageDataset(Dataset):
 
 
 
+
+
+class DoubleConv(nn.Module):
+    """(convolution => [BN] => ReLU) * 2"""
+
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+
+
+class Down(nn.Module):
+    """Downscaling with maxpool then double conv"""
+
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.maxpool_conv = nn.Sequential(
+            nn.MaxPool2d(2),
+            DoubleConv(in_channels, out_channels)
+        )
+
+    def forward(self, x):
+        return self.maxpool_conv(x)
+
+
+class Up(nn.Module):
+    """Upscaling then double conv"""
+
+    def __init__(self, in_channels, out_channels, bilinear=True):
+        super().__init__()
+
+        # if bilinear, use the normal convolutions to reduce the number of channels
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        else:
+            self.up = nn.ConvTranspose2d(in_channels // 2, in_channels // 2, kernel_size=2, stride=2)
+
+        self.conv = DoubleConv(in_channels, out_channels)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        diffY = torch.tensor([x2.size()[2] - x1.size()[2]])
+        diffX = torch.tensor([x2.size()[3] - x1.size()[3]])
+        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2])
+        x = torch.cat((x1,x2),1)
+        x = self.conv(x)
+        return x
+
+
+class OutConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(OutConv, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class UNet(nn.Module):
+    def __init__(self, n_channels, n_classes, bilinear=True):
+        super(UNet, self).__init__()
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.bilinear = bilinear
+
+        self.inc = DoubleConv(n_channels, 64)
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(256,256)
+        self.up1 = Up(512, 128, bilinear)
+        self.up2 = Up(256, 64, bilinear)
+        self.up3 = Up(128, 64, bilinear)
+        self.outc = OutConv(64, n_classes)
+        
+
+
+    def forward(self, x):
+        
+        
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x = self.down3(x3)
+        x = self.up1(x, x3)
+        x = self.up2(x, x2)
+        x = self.up3(x, x1)
+        logits = self.outc(x)
+        
+        if logits.shape[1]>1:
+            logits = F.softmax(logits,dim=1)
+            
+        else:
+            logits = torch.sigmoid(logits)
+
+        return logits
+
+
+class CompositionalLoss(nn.Module):
+    
+    
+    def __init__(self) -> None:
+        super(CompositionalLoss, self).__init__()
+        self.epsilon: float = 1e-6
+        self.dims = (1,2,3) 
+        
+    def forward(self,output,target):
+        
+       
+        loss = torch.pow(torch.pow(output - target,2) + self.epsilon,0.5)
+        
+        return torch.mean(loss)
