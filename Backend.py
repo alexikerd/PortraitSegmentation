e1429370a08f673b2218b24f22d9d7563ab379d0
alexikerd <41974772+alexikerd@users.noreply.github.com>
2020-07-11 17:06:28 -0500

    Add files via upload

--- Backend.py
+++ Backend.py
@@ -166,3 +166,112 @@ class CompositionalLoss(nn.Module):
         loss = torch.pow(torch.pow(output - target,2) + self.epsilon,0.5)
         
         return torch.mean(loss)
+
+
+class GluNet(nn.Module):
+    def __init__(self, bilinear=True):
+        super(GluNet, self).__init__()
+        self.bilinear = bilinear
+
+        self.inc = DoubleConv(3, 64)
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(256,512)
+        self.down4 = Down(512,512)
+        self.up1 = Up(1024, 256, bilinear)
+        self.up2 = Up(512, 128, bilinear)
+        self.up3 = Up(256,64, bilinear)
+        self.up4 = Up(128, 64, bilinear)
+        self.outc = OutConv(64,3)
+        
+        self.inc1 = DoubleConv(6, 64)
+        self.down1_1 = Down(64, 128)
+        self.down2_1 = Down(128, 256)
+        self.down3_1 = Down(256,512)
+        self.down4_1 = Down(512,512)
+        self.up1_1 = Up(1024, 256, bilinear)
+        self.up2_1 = Up(512, 128, bilinear)
+        self.up3_1 = Up(256,64, bilinear)
+        self.up4_1 = Up(128, 64, bilinear)
+        self.outc1 = OutConv(64,1)
+        
+
+
+    def forward(self, image):
+        
+        
+        
+        x1 = self.inc(image)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x4,x5)
+        x = self.up2(x,x3)
+        x = self.up3(x,x2)
+        x = self.up4(x,x1)
+        
+        trimap = F.softmax(self.outc(x),dim=1)
+        x = torch.cat((image,trimap),1)
+
+
+        x1 = self.inc1(x)
+        x2 = self.down1_1(x1)
+        x3 = self.down2_1(x2)
+        x4 = self.down3_1(x3)
+        x5 = self.down4_1(x4)
+        x = self.up1_1(x4,x5)
+        x = self.up2_1(x,x3)
+        x = self.up3_1(x,x2)
+        x = self.up4_1(x,x1)
+
+        matte = torch.sigmoid(self.outc1(x))
+
+        return trimap, matte
+
+
+
+
+class Cropper():
+    
+    def __init__(self):
+        
+        self.transform = transforms.Compose([transforms.ColorJitter(brightness=0.75),transforms.ToTensor(),transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+        self.root = path.abspath(path.curdir)        
+        self.device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+        self.imgsize = (600,800)
+        
+        self.TNet = UNet(3,3)
+        self.TNet.load_state_dict(torch.load(self.root + '/Models/Versions/COMP/TNET COMP v4'))
+#         self.TNet.to(self.device)
+        
+        self.MNet = UNet(6,1)
+        self.MNet.load_state_dict(torch.load(self.root + '/Models/Versions/COMP/MNET COMP v4'))
+#         self.MNet.to(self.device)
+        
+        
+    def __call__(self,image):
+        
+        self.TNet.to(self.device)
+        
+        image = image.resize(self.imgsize)
+        image = self.transform(image).unsqueeze(0).to(self.device)
+
+        with torch.no_grad():
+            trimap = self.TNet(image)
+            
+        self.TNet.cpu()
+        self.MNet.to(self.device)
+            
+        composite = torch.cat((image,trimap),1)
+        
+        with torch.no_grad():
+            matte = self.MNet(composite).cpu().squeeze().numpy()
+        
+        self.MNet.cpu()
+        
+        trimap = trimap.squeeze().cpu().numpy()
+        
+        fusion = trimap[0] + trimap[1]*matte
+        
+        return matte, fusion
